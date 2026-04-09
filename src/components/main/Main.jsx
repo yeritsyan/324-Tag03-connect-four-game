5d0cb0e9ed2573e70acf9c3fbcd792f67efa7006
Armen Yeritsyan <28714090+yeritsyan@users.noreply.github.com>
2026-04-09 21:41:19 +0200

    fixed title

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -9,7 +9,7 @@ const Main = ({ setCpu }) => {
   return (
     <div className="main__container buttons__container">
       <div className="main__content">
-        <h1>{GenerateTitle()} HELLO</h1>
+        <h1>{GenerateTitle()}</h1>
         <img src={LOGO} className="main__logo" alt="logo"></img>
         <Link
           to={"/game"}
