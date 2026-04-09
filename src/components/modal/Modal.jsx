ffe4e122b4d1178a803a4e96d5ebab5544aeca7c
28714090+yeritsyan@users.noreply.github.com <armen.yeritsyan@adesso.ch>
2026-04-09 19:18:16 +0200

    initial commit with full app

---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom";
+import "./modal.css";
+
+const Modal = ({ children, setPause }) => {
+  return ReactDOM.createPortal(
+    <div className="modal__container" onClick={() => setPause(false)}>
+      {children}
+    </div>,
+    document.getElementById("modal")
+  );
+};
+
+export { Modal };
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), svgr()],
+  base: process.env.NODE_ENV === "production" ? "/connect-four-game/" : "/",
+  server: {
+    port: 5173, // Default port, change if needed
+    open: true, // Automatically opens the browser on start
+  },
+  build: {
+    outDir: "dist", // Default output directory for production builds
+    sourcemap: true, // Useful for debugging
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`, // Ensures React is available in JSX files (optional)
+  },
+});
